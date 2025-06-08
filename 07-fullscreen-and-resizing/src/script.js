273636d1bba6976d7b60be7f67a5a461b5b70369
Fuka <fuukagogo0215@gmail.com>
2025-06-09 08:32:59 +0900

    add: 24:03

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -10,6 +10,9 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 /**
  * Object
  */
@@ -22,9 +25,22 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight,
 }
+
+window.addEventListener('resize', () => {
+    //Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+})
 
 /**
  * Camera
@@ -45,6 +61,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
